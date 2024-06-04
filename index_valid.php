dd934a69e33de351a2d65fb4a79368f45ce64f78
Vincenzo Pirozzi <vincenzopirozzi.applications@gmail.com>
2024-06-05 00:27:58 +0200

    Corretto SQL injection in index_valid.php

--- index_valid.php
+++ index_valid.php
@@ -1,20 +1,16 @@
 <?php
 require_once 'db.php';
-if(isset($_POST['addtask'])){
-  $task_add = $_POST['textfield'];
-  if(!empty($task_add)){
-    $task_add_query = "INSERT INTO task_table (task_name) VALUES  ('$task_add')";
-    $add_query = $dbcon -> query($task_add_query);
+if (isset($_POST['addtask'])) {
+  $task = $_POST['textfield'];
+  $stmt = $dbcon -> prepare("INSERT INTO task_table (task_name) VALUES (?)");
+  $stmt->bind_param("s", $task);
+  $stmt->execute();
+  if ($stmt->affected_rows > 0) {
+
+    $_SESSION['update_success'] = "Task added successfully!";
+  } else {
+    echo "Error: " . $stmt->error;
   }
-
-  header('location: index.php');
-  
-
-  
-
+  $stmt->close();
 }
-
-
-
-
 ?>
